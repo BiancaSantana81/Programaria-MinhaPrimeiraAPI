0a547ec257c58064640825bcec368372f5b3e9da
Bianca Santana Souza <bsantana@c2r3p4.42sp.org.br>
2024-09-16 18:28:31 -0300

    module 06 - 6.1

--- women.js
+++ women.js
@@ -1,7 +1,11 @@
-/* Importar a biblioteca express para trabalhar com back-end*/
+/* Importar a biblioteca express*/
 
 const express = require("express");
 const app = express();
+
+/* Importar a biblioteca UUID*/
+
+const {v4: uuidv4} = require('uuid');
 
 /* Constantes: qual a porta e criar rota para servidor */
 
@@ -12,16 +16,21 @@ const door = 3333;
 
 const women = [
     {
+        id: '1',
         name: 'Bianca Santana',
         image: 'https://avatars.githubusercontent.com/u/76911791?v=4',
         minibio: 'Desenvolvedora C / C++'
     },
     {
+        id: '2',
         name: 'Simara Conceição',
         image: 'https://avatars.githubusercontent.com/u/50921892?v=4',
         bio: 'Desenvolvedora e Instrutora'
     }
 ]
+
+women.push(newWoman);
+response.json(women);
 
 /* Função para identificar qual a porta usada pelo server */
 
@@ -30,17 +39,37 @@ function identifyDoor()
     console.log("Server created successfully! Open door: ", door);
 }
 
-/* Função para mandar uma resposta ao servidor */
+/* GET: Função para mandar uma resposta ao servidor */
 
-function addWomen(request, response)
+function viewWomen(request, response)
 {
     response.json(women);
 }
+
+/* POST: Função para criar uma nova mulher com POST */
+
+function addWoman(request, response)
+{ 
+    const newWoman = {
+    id : uuidv4(),
+    name: request.body.name,
+    image: request.body.image,
+    bio: request.body.bio
+    }
+
+    women.push(newWoman);
+    response.json(women);
+}
+
 
 /* Inicializar o servidor pela porta escolhida */
 
 app.listen(door, identifyDoor);
 
-/* Realizar uma requisição para o servidor */
+/* Realizar uma requisição para o servidor - GET */
 
-app.use(router.get('/women', addWomen));
+app.use(router.get('/women', viewWomen));
+
+/* POST */
+
+app.use(router.post('/women', addWoman));
